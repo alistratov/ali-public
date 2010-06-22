b1ba271627f900d66781bee0cfcec279054cb44b
Oleg Alistratov <ali-reg@ya.ru>
2010-06-22 23:20:09 +0300

    Added MANIFEST.SKIP

--- modules/Data-Password-Entropy/Build.bat
+++ modules/Data-Password-Entropy/Build.bat
@@ -24,7 +24,7 @@ sub magic_number_matches {
   open FH, '_build\\magicnum' or return 0;
   my $filenum = <FH>;
   close FH;
-  return $filenum == 440093;
+  return $filenum == 591735;
 }
 
 my $progname;
